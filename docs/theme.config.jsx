160ab4bc45bf1a94b1b41b3dd011cd3a60ff674b
sha256 <shamim@hasnath.net>
2023-01-19 19:02:06 +0600

    Fix head tags in docs

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -3,12 +3,18 @@ export default {
   project: {
     link: 'https://github.com/sha256/Mongur',
   },
-  docsRepositoryBase: 'https://github.com/sha256/Mongur/docs',
+  docsRepositoryBase: 'https://github.com/sha256/Mongur',
   useNextSeoProps() {
     return {
       titleTemplate: '%s – Mongur'
     }
   },
+  head: (
+    <>
+      <meta property="og:title" content="Mongur - Typscript ODM for MongoDB" />
+      <meta property="og:description" content="Define MongoDB models and query data using Typescript/Javascript classes." />
+    </>
+  ),
   footer: {
     text: <span></span>,
   }
